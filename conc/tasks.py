837ca39b9879e3514e051707fe819d5681c8dbdf
dauglas <1729589971@qq.com>
2016-08-22 22:50:09 +0800

    commit code

--- conc/tasks.py
+++ conc/tasks.py
@@ -3,8 +3,8 @@ from __future__ import absolute_import
 from robot_web.celery import app
 from celery import shared_task
 from channels import Group
+from scrapy.cmdline import execute
 import time
-
 
 # @app.task
 @shared_task
@@ -19,9 +19,14 @@ def add(x, y):
 
 
 @app.task
-def sendTest():
+def send_test():
     FORMAT = '%Y-%m-%d %X'
     now = time.strftime(FORMAT, time.localtime())
     Group("conc").send({
         "text": "today is " + now
     })
+
+
+@app.task
+def crawl_jin10():
+    execute(['scrapy', 'crawl', 'jin10'])
